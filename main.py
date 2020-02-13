1dfb038a6945340022a46e9ea5a8fda14926aa48
rithvik213 <33884560+rithvik213@users.noreply.github.com>
2020-02-13 10:14:17 -0500

    Cesar Cipher with while loop

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-highest = 0
-lowest = 10
-total = 0
-counter = 0
-exitKey = 'y'
-overallAvg = 0
-
-while exitKey == 'y' or exitKey == 'Y':
-  counter+=1
-  highest = 0
-  lowest = 10
-  total = 0
-  
-  for x in range(5):
-    number = float(input("Enter a number"))
-    while number < 0 or number > 10:
-      number = float(input("Invalid Input! Please Reenter: "))
-    total += number
-    if number < lowest:
-      lowest = number
-    elif number > highest:
-      highest = number
-  
-  print "Highest score is", highest,"Lowest score is", lowest
-  avg = (total - (highest+ lowest)) / 3
-  print "Average is", avg
-  print "Counter is:",counter
-  overallAvg += avg
-  exitKey = input("Would you like to continue? (Type y or n)")
-
-print "Final Average is: ", (overallAvg / counter)
+conversion = input("Are you encrypting or decrypting?")
+test = input("What's the secret message?")
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+cipherMessage = ""
+key = int(input("What is they key?"))
+x = 0
+while x < len(test):
+  whereisletter = alphabet.find(test[x])
+  if conversion == "encrypting" or conversion == "Encrypting":
+    newletter = alphabet[(whereisletter + key)%26]
+  else:
+    newletter = alphabet[(whereisletter - key)%26]
+  cipherMessage = cipherMessage + newletter
+  x = x + 1
+print cipherMessage
